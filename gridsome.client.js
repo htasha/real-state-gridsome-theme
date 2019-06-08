ac4109ca34854ed1f3d9e515ac61a8e341c44dce
ahsath <ahsath@gmail.com>
2019-06-07 21:55:38 -0500

    Imported new icons

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,10 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { config, library } from '@fortawesome/fontawesome-svg-core';
-import { faPhone, faBed, faBath, faVectorSquare, faCrown } from '@fortawesome/free-solid-svg-icons';
+import { faPhone, faBed, faBath, faVectorSquare, faCrown, faBuilding, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
 config.autoAddCss = false;
-library.add(faPhone, faBed, faBath, faVectorSquare, faCrown);
+library.add(faPhone, faBed, faBath, faVectorSquare, faCrown, faBuilding, faMapMarkedAlt);
 
 export default function (Vue) {
     Vue.component('font-awesome', FontAwesomeIcon)
