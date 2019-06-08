93c96c201291eeb549b2aa4f4f12722f4d30c374
ahsath <ahsath@gmail.com>
2019-06-08 01:11:02 -0500

    Minor markup update

--- src/components/PropertiesFeatureCards.vue
+++ src/components/PropertiesFeatureCards.vue
@@ -7,21 +7,27 @@
             <font-awesome icon="building" size="2x" class="text-primary"></font-awesome>
           </div>
           <b-card-title title-tag="h5">Amplia gama de Apartamentos</b-card-title>
-          <b-card-text>Alquiler por días, cortas y largas temporadas a precios económicos. Consultar disponibilidad.</b-card-text>
+          <b-card-text
+            class="text-muted"
+          >Alquiler por días, cortas y largas temporadas a precios económicos. Consultar disponibilidad.</b-card-text>
         </b-card>
         <b-card>
           <div class="pb-3">
             <font-awesome icon="crown" size="2x" class="text-primary"></font-awesome>
           </div>
           <b-card-title title-tag="h5">Estrato 5</b-card-title>
-          <b-card-text>Apartamentos estrato 5 ubicados cerca de almacenes de cadena, centros comerciales y un excelente medio de transporte aledaño a universidades.</b-card-text>
+          <b-card-text
+            class="text-muted"
+          >Apartamentos estrato 5 ubicados cerca de almacenes de cadena, centros comerciales y un excelente medio de transporte aledaño a universidades.</b-card-text>
         </b-card>
         <b-card>
           <div class="pb-3">
             <font-awesome icon="map-marked-alt" size="2x" class="text-primary"></font-awesome>
           </div>
           <b-card-title title-tag="h5">Atracciones Turísticas</b-card-title>
-          <b-card-text>Situados a 10 minutos en carro del Pueblo Paisa, a 5 minutos del estadio y 15 minunots del centro de la ciudad, 8 min en carro a la estación Floresta del metro y mucho más.</b-card-text>
+          <b-card-text
+            class="text-muted"
+          >Situados a 10 minutos en carro del Pueblo Paisa, a 5 minutos del estadio y 15 minunots del centro de la ciudad, 8 min en carro a la estación Floresta del metro y mucho más.</b-card-text>
         </b-card>
       </the-grid>
     </b-container>
