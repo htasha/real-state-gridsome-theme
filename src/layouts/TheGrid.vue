cad450ad947e4aa222befb75fc40458ab841e8a5
ahsath <ahsath@gmail.com>
2019-06-07 21:56:36 -0500

    Added TheGrid component

---
+++ src/layouts/TheGrid.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="grid">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(auto, 350px));
+  gap: 1.5rem;
+  justify-content: center;
+}
+</style>
