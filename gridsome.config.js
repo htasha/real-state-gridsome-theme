011177a23bea296edfc539a5f29dfa385945d96b
ahsath <ahsath@gmail.com>
2019-06-08 20:37:36 -0500

    Added Gridsome source-filesystem/transformer-remark plugins

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'properties/*.md',
+        typeName: 'Property',
+        route: '/apartamentos/:slug',
+      }
+    }
+  ]
 }
