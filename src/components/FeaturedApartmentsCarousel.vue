51e26f3f256b4ba4272dc32448841884617bbbee
ahsath <ahsath@gmail.com>
2019-06-08 20:42:45 -0500

    Added featuredProperties prop + markup update

--- src/components/FeaturedApartmentsCarousel.vue
+++ src/components/FeaturedApartmentsCarousel.vue
@@ -1,18 +1,27 @@
 <template>
-  <b-carousel :interval="0" controls fade>
-    <b-carousel-slide img-alt="Blank image" class="vh-100">
+  <b-carousel :interval="5000" controls fade>
+    <b-carousel-slide
+      v-for="{ node: { id, path, featuredImage, featuredImageAltText, title, address } } in featuredProperties"
+      :key="id"
+      class="vh-100"
+    >
       <template #img>
-        <g-image
-          src="~/assets/images/apartamento-1-camas-1.jpg"
-          quality="100"
-          class="carousel__image w-100 h-100"
-        ></g-image>
+        <g-link :to="path">
+          <g-image
+            :src="featuredImage"
+            quality="100"
+            class="carousel__image w-100 h-100"
+            :alt="featuredImageAltText"
+          ></g-image>
+        </g-link>
       </template>
       <div class="d-flex justify-content-end text-left">
         <b-card class="caption__card" no-body>
           <b-card-body body-class="p-3">
-            <b-card-title class="text-dark" title-tag="h5">Apartamento sector Los Laureles</b-card-title>
-            <b-card-sub-title>Merrick Way, Miami, FL 33134, USA</b-card-sub-title>
+            <g-link to="/" class="text-decoration-none">
+              <b-card-title class="text-dark" title-tag="h5">{{ title }}</b-card-title>
+            </g-link>
+            <b-card-sub-title>{{ address }}</b-card-sub-title>
           </b-card-body>
           <b-card-body body-class="text-dark pt-0 px-3 pb-2">
             <h6 class="font-weight-normal">Renta</h6>
@@ -27,6 +36,12 @@
 <script>
 import { BCarousel, BCarouselSlide, BCard, BCardText } from "bootstrap-vue";
 export default {
+  props: {
+    featuredProperties: {
+      type: Array,
+      required: true
+    }
+  },
   components: {
     BCarousel,
     BCarouselSlide,
