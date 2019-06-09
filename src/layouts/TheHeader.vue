e4f2887b82b9988954495009154922f7b0771db5
ahsath <ahsath@gmail.com>
2019-06-08 20:44:36 -0500

    Minor update to TheHeader

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -1,25 +1,21 @@
 <template>
   <b-navbar toggleable="sm" fixed="top" type="dark" variant="primary">
-    <b-container>
-      <b-navbar-brand>
-        <g-link to="/">Logo.</g-link>
-      </b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item>Apartamentos</b-nav-item>
-          <b-nav-item>Servicios</b-nav-item>
-          <b-nav-item>Contacto</b-nav-item>
-        </b-navbar-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-dropdown-divider></b-dropdown-divider>
-          <b-nav-text>
-            <font-awesome icon="phone" flip="horizontal" pull="left" size="lg"></font-awesome>
-            <b-link href="tel:18005551234">1-800-555-1234</b-link>
-          </b-nav-text>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-container>
+    <b-navbar-brand to="/">Some fancy logo</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse" class="border-0"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item>Apartamentos</b-nav-item>
+        <b-nav-item>Servicios</b-nav-item>
+        <b-nav-item>Contacto</b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-nav-text>
+          <font-awesome icon="phone" flip="horizontal" pull="left" size="lg"></font-awesome>
+          <b-link href="tel:18005551234">1-800-555-1234</b-link>
+        </b-nav-text>
+      </b-navbar-nav>
+    </b-collapse>
   </b-navbar>
 </template>
 
