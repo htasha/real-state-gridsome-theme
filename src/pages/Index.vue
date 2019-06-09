8e6cebf0622c0ccd5f21784b688300ae6ee9e436
ahsath <ahsath@gmail.com>
2019-06-08 20:45:07 -0500

    Added <page-query> component for fetching data

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,25 +1,43 @@
 <template>
   <div>
     <the-header></the-header>
-    <featured-apartments-carousel></featured-apartments-carousel>
+    <featured-apartments-carousel :featured-properties="$page.properties.edges"></featured-apartments-carousel>
     <properties-feature-cards></properties-feature-cards>
     <properties-grid-layout></properties-grid-layout>
+    <the-freaking-footer></the-freaking-footer>
   </div>
 </template>
+
+<page-query>
+query AllProperties {
+  properties: allProperty (filter: { featured: { eq: true } }) {
+    edges {
+      node {
+        id
+        path
+        featuredImage
+        featuredImageAltText
+        title
+        address
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 import FeaturedApartmentsCarousel from "~/components/FeaturedApartmentsCarousel";
 import PropertiesGridLayout from "~/components/PropertiesGridLayout";
 import PropertiesFeatureCards from "~/components/PropertiesFeatureCards.vue";
 export default {
-  name: "App",
+  name: "IndexPage",
   components: {
     FeaturedApartmentsCarousel,
     PropertiesGridLayout,
     PropertiesFeatureCards
   },
   metaInfo: {
-    title: "Hello, world!"
+    title: "Some fancy brand name"
   }
 };
 </script>
