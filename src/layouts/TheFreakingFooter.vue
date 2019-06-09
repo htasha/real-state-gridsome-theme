b9a291e0da8374e2b0e0e598a4e26475c692fd69
ahsath <ahsath@gmail.com>
2019-06-08 20:44:00 -0500

    Added TheFreakingFooter component

---
+++ src/layouts/TheFreakingFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="bg-light py-5 border-top">
+    <b-container>
+      <b-row>
+        <b-col>
+          <h1>Some fancy logo</h1>
+        </b-col>
+        <b-col>image</b-col>
+        <b-col>menu</b-col>
+        <b-col>la empresa</b-col>
+        <b-col>contactanos</b-col>
+        <b-col>copy</b-col>
+      </b-row>
+    </b-container>
+  </footer>
+</template>
