bc2b4639847033d621601d8e7e2d2d8505447937
ahsath <ahsath@gmail.com>
2019-06-09 20:56:39 -0500

    Minor markup update

--- src/components/FeaturedApartmentsCarousel.vue
+++ src/components/FeaturedApartmentsCarousel.vue
@@ -18,9 +18,9 @@
       <div class="d-flex justify-content-end text-left">
         <b-card class="caption__card" no-body>
           <b-card-body body-class="p-3">
-            <g-link to="/" class="text-decoration-none">
-              <b-card-title class="text-dark" title-tag="h5">{{ title }}</b-card-title>
-            </g-link>
+            <b-card-title title-tag="h5">
+              <g-link :to="path" class="text-dark text-decoration-none">{{ title }}</g-link>
+            </b-card-title>
             <b-card-sub-title>{{ address }}</b-card-sub-title>
           </b-card-body>
           <b-card-body body-class="text-dark pt-0 px-3 pb-2">
