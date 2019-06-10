76c677e31132e6290a098d0a50e5bd2663354d6a
ahsath <ahsath@gmail.com>
2019-06-09 20:56:11 -0500

    Removed unused component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import { BContainer, BRow, BCol, BCard, BCardText, BCardBody, BCardTitle, BCardSubTitle, BCardFooter, ListGroupPlugin } from 'bootstrap-vue';
+import { BContainer, BRow, BCol, BCard, BCardText, BCardBody, BCardTitle, BCardSubTitle, ListGroupPlugin, BButton } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import '~/assets/scss/custom.scss';
@@ -11,7 +11,7 @@ import TheHeader from '~/layouts/TheHeader.vue';
 import TheGrid from '~/layouts/TheGrid.vue';
 import TheFreakingFooter from '~/layouts/TheFreakingFooter.vue';
 
-export default function (Vue, { router, head, isClient }) {
+export default function (Vue, { router }, head, isClient) {
   // Framework components
   Vue.use(ListGroupPlugin);
   Vue.component('BContainer', BContainer);
@@ -22,7 +22,7 @@ export default function (Vue, { router, head, isClient }) {
   Vue.component('BCardBody', BCardBody);
   Vue.component('BCardTitle', BCardTitle);
   Vue.component('BCardSubTitle', BCardSubTitle);
-  Vue.component('BCardFooter', BCardFooter);
+  Vue.component('BButton', BButton);
   // In house components
   Vue.component('TheHeader', TheHeader);
   Vue.component('TheGrid', TheGrid);
