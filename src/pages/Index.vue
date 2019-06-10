414bdd931bbb5d66c893900dc1ae12c55f5bc1ac
ahsath <ahsath@gmail.com>
2019-06-09 20:58:30 -0500

    Added loadMore method

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
     <the-header></the-header>
-    <featured-apartments-carousel :featured-properties="$page.properties.edges"></featured-apartments-carousel>
+    <featured-apartments-carousel :featured-properties="$page.featuredProperties.edges"></featured-apartments-carousel>
     <properties-feature-cards></properties-feature-cards>
-    <properties-grid-layout></properties-grid-layout>
+    <properties-grid-layout :all-properties="nodes"></properties-grid-layout>
+    <b-button variant="primary" @click="loadMore">Cargar más</b-button>
     <the-freaking-footer></the-freaking-footer>
   </div>
 </template>
 
 <page-query>
-query AllProperties {
-  properties: allProperty (filter: { featured: { eq: true } }) {
+query AllProperties ($page: Int) {
+  featuredProperties: allProperty (filter: { featured: { eq: true } }) {
     edges {
       node {
         id
@@ -19,6 +20,26 @@ query AllProperties {
         featuredImageAltText
         title
         address
+      }
+    }
+  }
+  allProperties: allProperty (perPage: 1, page: $page) @paginate {
+    pageInfo {
+      currentPage
+      totalPages
+    }
+    edges {
+      node {
+        id
+        path
+        featuredImage (width:350, height: 200, quality: 90)
+        featuredImageAltText
+        title
+        address
+        stratum
+        bedrooms
+        bathrooms
+        area
       }
     }
   }
@@ -31,13 +52,38 @@ import PropertiesGridLayout from "~/components/PropertiesGridLayout";
 import PropertiesFeatureCards from "~/components/PropertiesFeatureCards.vue";
 export default {
   name: "IndexPage",
+  data() {
+    return {
+      totalPages: 0,
+      currentPage: 0,
+      nodes: []
+    };
+  },
   components: {
     FeaturedApartmentsCarousel,
     PropertiesGridLayout,
     PropertiesFeatureCards
   },
+  methods: {
+    async loadMore() {
+      try {
+        if (this.currentPage === this.totalPages) return;
+        const results = await this.$fetch(`/${++this.currentPage}`);
+        results.data.allProperties.edges.forEach(node => {
+          this.nodes.push(node);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
   metaInfo: {
     title: "Some fancy brand name"
+  },
+  mounted() {
+    this.nodes.push(this.$page.allProperties.edges[0]);
+    this.totalPages = this.$page.allProperties.pageInfo.totalPages;
+    this.currentPage = this.$page.allProperties.pageInfo.currentPage;
   }
 };
 </script>
