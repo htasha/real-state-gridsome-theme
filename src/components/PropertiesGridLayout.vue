0a6d3761dff2a0bfe73707892dfb6702c4a93f42
ahsath <ahsath@gmail.com>
2019-06-09 20:57:18 -0500

    Moved to PropertiesGridLayout compoent

--- src/components/PropertiesGridLayout.vue
+++ src/components/PropertiesGridLayout.vue
@@ -7,38 +7,88 @@
           <h2>Apartamentos</h2>
           <p class="text-muted">Echa un vistazo a algunos de nuestros últimos inmuebles</p>
         </div>
-        <properties-features>
-          <g-image class="card-img" src="~/assets/images/apartamento-1-camas-1.jpg"></g-image>
+        <b-card
+          v-for="{ node: { id, path, featuredImage, featuredImageAltText, title, address, stratum, bedrooms, bathrooms, area } } in allProperties"
+          :key="id"
+          no-body
+        >
+          <g-link :to="path">
+            <g-image class="card-img" :src="featuredImage" :alt="featuredImageAltText"></g-image>
+          </g-link>
           <b-card-body body-class="p-3">
-            <b-card-title title-tag="h5">Apartamento sector Los Laureles</b-card-title>
-            <b-card-sub-title>Merrick Way, Miami, FL 33134, USA</b-card-sub-title>
+            <b-card-title title-tag="h5">
+              <g-link :to="path" class="text-dark text-decoration-none">{{ title }}</g-link>
+            </b-card-title>
+            <b-card-sub-title>{{ address }}</b-card-sub-title>
           </b-card-body>
-        </properties-features>
-        <properties-features>
-          <g-image class="card-img" src="~/assets/images/apartamento-1-camas-1.jpg"></g-image>
-          <b-card-body body-class="p-3">
-            <b-card-title title-tag="h5">Apartamento sector Los Laureles</b-card-title>
-            <b-card-sub-title>Merrick Way, Miami, FL 33134, USA</b-card-sub-title>
-          </b-card-body>
-        </properties-features>
-        <properties-features>
-          <g-image class="card-img" src="~/assets/images/apartamento-1-camas-1.jpg"></g-image>
-          <b-card-body body-class="p-3">
-            <b-card-title title-tag="h5">Apartamento sector Los Laureles</b-card-title>
-            <b-card-sub-title>Merrick Way, Miami, FL 33134, USA</b-card-sub-title>
-          </b-card-body>
-        </properties-features>
+          <b-list-group class="border-top border-bottom">
+            <b-list-group-item
+              class="border-0 d-flex align-items-center justify-content-between"
+              button
+            >
+              <div class="d-flex align-items-center">
+                <div class="properties-list__icon mr-3">
+                  <font-awesome icon="crown" class="text--small text-gray-light"></font-awesome>
+                </div>
+                <h6 class="mb-0 font-weight-normal">Estrato</h6>
+              </div>
+              <h6 class="mb-0 font-weight-normal">{{ stratum }}</h6>
+            </b-list-group-item>
+            <b-list-group-item
+              class="border-0 d-flex align-items-center justify-content-between"
+              button
+            >
+              <div class="d-flex align-items-center">
+                <div class="properties-list__icon mr-3">
+                  <font-awesome icon="bed" class="text--small text-gray-light"></font-awesome>
+                </div>
+                <h6 class="mb-0 font-weight-normal">Habitaciones</h6>
+              </div>
+              <h6 class="mb-0 font-weight-normal">{{ bedrooms }}</h6>
+            </b-list-group-item>
+            <b-list-group-item
+              class="border-0 d-flex align-items-center justify-content-between"
+              button
+            >
+              <div class="d-flex align-items-center">
+                <div class="properties-list__icon mr-3">
+                  <font-awesome icon="bath" class="text--small text-gray-light"></font-awesome>
+                </div>
+                <h6 class="mb-0 font-weight-normal">Baños</h6>
+              </div>
+              <h6 class="mb-0 font-weight-normal">{{ bathrooms }}</h6>
+            </b-list-group-item>
+            <b-list-group-item
+              class="border-0 d-flex align-items-center justify-content-between"
+              button
+            >
+              <div class="d-flex align-items-center">
+                <div class="properties-list__icon mr-3">
+                  <font-awesome icon="vector-square" class="text--small text-gray-light"></font-awesome>
+                </div>
+                <h6 class="mb-0 font-weight-normal">Área</h6>
+              </div>
+              <h6 class="mb-0 font-weight-normal">{{ area }} &#13217;</h6>
+            </b-list-group-item>
+          </b-list-group>
+          <b-list-group-item class="border-0">
+            <h6 class="font-weight-normal">Renta</h6>
+            <h5 class="text-primary">$ 100,000 / mes</h5>
+          </b-list-group-item>
+        </b-card>
       </the-grid>
     </b-container>
   </section>
 </template>
 
 <script>
-import PropertiesFeatures from "~/components/PropertiesFeatures.vue";
 export default {
   name: "PropertiesGridLayout",
-  components: {
-    PropertiesFeatures
+  props: {
+    allProperties: {
+      type: Array,
+      required: true
+    }
   }
 };
 </script>
@@ -46,6 +96,10 @@ export default {
 <style>
 .grid__header {
   grid-column: 1 / -1;
+}
+.properties-list__icon {
+  width: 24px;
+  height: 24px;
 }
 </style>
 
