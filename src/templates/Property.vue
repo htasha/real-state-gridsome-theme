5fca4fa67e8fcd256e78acd234ceec49884e0719
ahsath <ahsath@gmail.com>
2019-06-17 15:02:16 -0500

    Markup + graphql update

--- src/templates/Property.vue
+++ src/templates/Property.vue
@@ -2,13 +2,44 @@
   <div class="property">
     <the-header></the-header>
     <div class="property__grid">
-      <div class="property__img">
-        <g-image :src="$page.property.featuredImage" class="w-100 hero__img"/>
+      <g-image
+        :src="$page.property.featuredImage"
+        :alt="$page.property.featuredImageAltText"
+        class="w-100 hero__img"
+      />
+      <div class="property__gradient">
+        <b-container class="property__title">
+          <h2 class="text-white py-3 mb-0 font-weight-normal">{{$page.property.title}}</h2>
+        </b-container>
       </div>
-      <b-container class="property__title">
-        <h1 class="text-white mb-4 font-weight-normal">{{$page.property.title}}</h1>
-      </b-container>
     </div>
+    <section class="py-5">
+      <b-container>
+        <b-row>
+          <b-col lg="8">
+            <span class="d-flex align-items-center mb-3">
+              <h4 class="font-weight-normal mb-0 mr-2">{{ $page.property.title }}</h4>
+              <b-badge variant="primary">Venta</b-badge>
+            </span>
+            <span class="d-flex align-items-center">
+              <font-awesome icon="map-marker-alt" class="text-muted"></font-awesome>
+              <h6 class="text-muted font-weight-normal mb-0 ml-2">{{ $page.property.address }}</h6>
+            </span>
+            <b-carousel :interval="5000" controls fade class="mt-5">
+              <b-carousel-slide
+                v-for="({ image, altText }, i) in $page.property.propertyImages"
+                :key="i"
+              >
+                <template #img>
+                  <g-image :src="image" quality="100" class="carousel__image w-100" :alt="altText"></g-image>
+                </template>
+              </b-carousel-slide>
+            </b-carousel>
+          </b-col>
+          <b-col></b-col>
+        </b-row>
+      </b-container>
+    </section>
   </div>
 </template>
 
@@ -16,15 +47,25 @@
 query Properties($path: String) {
   property(path: $path) {
     id
-    featuredImage (width: 1366, quality: 90)
+    featuredImage (width: 1366, quality: 85)
+    featuredImageAltText
     title
+    address
+    propertyImages {
+      image (height: 350, width: 500, quality: 100)
+      altText
+    }
   }
 }
 </page-query>
 
 <script>
+import { BBadge } from "bootstrap-vue";
+[{ image: {}, altText: "alttext" }];
 export default {
-  mounted() {}
+  components: {
+    BBadge
+  }
 };
 </script>
 
@@ -35,17 +76,21 @@ export default {
 .property__grid {
   display: grid;
 }
+.property__gradient {
+  background-image: -webkit-linear-gradient(rgba(0, 0, 0, 0), #000);
+  background-image: linear-gradient(rgba(0, 0, 0, 0), #000);
+}
 .property__img {
   min-height: 300px;
   max-height: 350px;
   z-index: 1;
 }
-.property__title {
+.property__gradient {
   align-self: end;
   z-index: 2;
 }
-.property__title,
-.property__img {
+.property__gradient,
+.hero__img {
   grid-column: 1 / -1;
   grid-row: 1 / 2;
 }
