4db0f0c4ebcfce5047db608bc561bee2690fb689
ahsath <ahsath@gmail.com>
2019-06-17 21:15:00 -0500

    Updated

--- gridsome.config.js
+++ gridsome.config.js
@@ -13,6 +13,7 @@ module.exports = {
         path: 'properties/*.md',
         typeName: 'Property',
         route: '/apartamento/:slug',
+        resolveAbsolutePaths: true
       }
     },
     {
