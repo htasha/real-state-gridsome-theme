f935705339375305bd1a8e10f70db073ebbdd3be
ahsath <ahsath@gmail.com>
2019-06-20 17:45:05 -0500

    Imported new components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import { BContainer, BRow, BCol, BCard, BCardText, BCardBody, BCardTitle, BCardSubTitle, ListGroupPlugin, BButton, BLink, BCarousel, BCarouselSlide } from 'bootstrap-vue';
+import { BContainer, BRow, BCol, BCard, BCardText, BCardBody, BCardTitle, BCardSubTitle, ListGroupPlugin, BButton, BLink, BCarousel, BCarouselSlide, BForm, BFormGroup, BFormInput, BFormTextarea } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import '~/assets/scss/custom.scss';
@@ -25,9 +25,13 @@ export default function (Vue, { router }, head, isClient) {
   Vue.component('BButton', BButton);
   Vue.component('BCarousel', BCarousel);
   Vue.component('BCarouselSlide', BCarouselSlide);
+  Vue.component('BForm', BForm);
+  Vue.component('BFormInput', BFormInput);
+  Vue.component('BFormGroup', BFormGroup);
+  Vue.component('BFormTextarea', BFormTextarea);
+  Vue.component('BLink', BLink);
   // In house components
   Vue.component('TheHeader', TheHeader);
   Vue.component('TheGrid', TheGrid);
   Vue.component('TheFreakingFooter', TheFreakingFooter);
-  Vue.component('BLink', BLink);
 }
